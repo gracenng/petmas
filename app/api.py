f8896c03e0e4fed58eae94771a9851137a096453
Grace Nguyen <42276283+gracenng@users.noreply.github.com>
2021-01-04 20:48:57 -0500

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,6 +1,22 @@
 from flask import Flask
+import pyodbc
 app = Flask(__name__)
 
-@app.route('/petmas')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/api/petmas')
+def getUrl():
+    # Connect to sql server
+    server = 'tcp: petmas-server.database.windows.net, 1433'
+    database = 'petmasdb'
+    username = '<REDACTED>'
+    password = '<REDACTED>'
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=' +
+                          server+';DATABASE='+database+';Encrypt=yes;UID='+username+';PWD=' + password)
+
+    cursor = conn.cursor()
+    # q = (''' SELECT url FROM links''')
+    result = cursor.execute(' SELECT url FROM links')
+    return str(result.fetchall())
+
+
+app.run()
